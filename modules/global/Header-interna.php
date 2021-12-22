c430a0a4d1b7c32e47b257e5076c140f294a78a4
lucasVictor <lucas.v0697@gmail.com>
2021-12-22 13:45:50 -0300

    Header images Single and Categories

--- modules/global/Header-interna.php
+++ modules/global/Header-interna.php
@@ -4,9 +4,23 @@ if (!defined('PATH')) exit;
 
 $elem = is_single() ? 'h2' : 'h1';
 
+if (is_page() or is_single()) {
+  $hi = (object) [
+    'desktop' => wpfx_get_image(get_field('hi-desktop')),
+    'mobile' => wpfx_get_image(get_field('hi-mobile')),
+  ];
+}
+
+elseif (is_category()) {
+  $hi = (object) [
+    'desktop' => z_taxonomy_image_url(),
+    'mobile' => z_taxonomy_image_url(),
+  ];
+}
+
 ?>
 
-<section class="header-interna">
+<section class="header-interna background-parallax-ctrl">
 
   <div class="container">
     <<?=$elem?>>
@@ -23,3 +37,21 @@ $elem = is_single() ? 'h2' : 'h1';
   </div>
 
 </section>
+
+<?php
+
+if ($hi->desktop or $hi->mobile) {
+?>
+  <style>
+    section.header-interna {
+      background-image: url('<?= $hi->mobile ?>');
+      background-size: cover;
+    }
+    @media (min-width: 768px) {
+      section.header-interna {
+        background-image: url('<?= $hi->desktop ?>');
+      }
+    }
+  </style>
+<?php
+} 
