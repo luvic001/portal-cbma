6682e497897af0a5794409d87a20fd3449ba73fd
lucasVictor <lucas.v0697@gmail.com>
2021-12-23 13:56:48 -0300

    Conteúdo do Editor Wordpress

--- modules/global/components/Content.php
+++ modules/global/components/Content.php
@@ -2,6 +2,9 @@
 
 if (!defined('PATH')) exit;
 
+if (get_the_content())
+  get_modules('components/Editor-Wordpress');
+
 if (have_rows('componentes')):
   while (have_rows('componentes')):
     the_row();
---
+++ modules/global/components/Editor-Wordpress.php
@@ -0,0 +1,15 @@
+<?php
+
+if (!defined('PATH')) exit;
+
+?>
+
+<section class="site-sections">
+
+  <div class="container">
+    <div class="editor-content editor-content-painel">
+      <?= get_the_content() ?>
+    </div>
+  </div>
+
+</section>
